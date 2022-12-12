03fc6ab43141977587bfd52913d5dae46d6cce34
Alessandro Cesar <81247428+santosalessandrocesar@users.noreply.github.com>
2022-12-12 00:14:00 -0300

    Trabalho faculdade

---
+++ dashboard.js
@@ -0,0 +1,60 @@
+/* globals Chart:false, feather:false */
+
+(() => {
+  'use strict'
+
+  feather.replace({ 'aria-hidden': 'true' })
+
+  // Graphs
+  const ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        'Janeiro',
+        'Fevereiro',
+        'Março',
+        'Abril',
+        'Maio',
+        'Junho',
+        'Julho',
+        'Agosto',
+        'Setembro',
+        'Outubro',
+        'Novembro',
+        'Dezembro'
+      ],
+      datasets: [{
+        data: [
+          15339,
+          21345,
+          18483,
+          24003,
+          23489,
+          24092,
+          12034,
+          21466,
+          43214
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#FF0000',
+        borderWidth: 3,
+        pointBackgroundColor: '#FF0000'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })
+})()
